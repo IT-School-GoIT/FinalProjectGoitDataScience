3b97ed57003af3ae458bd804980ce7b8adec6759
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-08-09 00:44:48 +0300

    privat & present

--- home/urls.py
+++ home/urls.py
@@ -7,5 +7,10 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("team/", views.team, name="team"),
     path("cognition/", views.cognition, name="cognition"),
-
+    path("privacy_policy/", views.privacy_policy, name="privacy_policy"),
+    path(
+        "presentation_of_the_project/",
+        views.presentation_of_the_project,
+        name="presentation_of_the_project",
+    ),
 ]
--- home/views.py
+++ home/views.py
@@ -20,3 +20,23 @@ def team(request):
     return render(
         request, "home/team.html", {"title": "Команда", "page": "team", "app": "home"}
     )
+
+
+def privacy_policy(request):
+    return render(
+        request,
+        "home/privacy_policy.html",
+        {"title": "Політика конфіденційності", "page": "privacy_policy", "app": "home"},
+    )
+
+
+def presentation_of_the_project(request):
+    return render(
+        request,
+        "home/presentation_of_the_project.html",
+        {
+            "title": "Презентація проєкту",
+            "page": "presentation_of_the_project",
+            "app": "home",
+        },
+    )
