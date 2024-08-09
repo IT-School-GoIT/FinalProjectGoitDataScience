51567da2e9da2883f4f8a179024acd93643a741c
ANTON BABENKO <antonbabenko1983@gmail.com>
2024-08-09 10:18:40 +0300

    Update settings.py

--- root/settings.py
+++ root/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # STATICFILES_DIRS = [BASE_DIR / 'static']
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
