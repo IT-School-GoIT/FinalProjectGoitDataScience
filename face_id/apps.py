2ea5df1085542c6972624884743f7a149ad53751
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-08-15 21:58:55 +0300

    del face id

--- face_id/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class FaceIdConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'face_id'
--- root/settings.py
+++ root/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'accounts',
-    'face_id',
+    
 ]
 
 MIDDLEWARE = [
