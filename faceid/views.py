05ebc9134b1c2f0d411043f7643c6c2a6cdf9938
bobantonbob <130400459+bobantonbob@users.noreply.github.com>
2024-08-23 11:18:00 +0300

    faceid fix

--- faceid/views.py
+++ faceid/views.py
@@ -1,5 +1,6 @@
 import io
 import face_recognition
+from PIL import Image
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
 from .models import UserPhoto
@@ -16,6 +17,18 @@ def index(request):
     return render(request, "accounts/signup.html")
 
 
+def resize_image(image_content, size=(400, 300)):
+    image = Image.open(io.BytesIO(image_content))
+
+    # Перевірка на альфа-канал і перетворення в RGB, якщо необхідно
+    if image.mode == 'RGBA':
+        image = image.convert('RGB')
+
+    resized_image = image.resize(size)
+    byte_arr = io.BytesIO()
+    resized_image.save(byte_arr, format='JPEG')
+    return byte_arr.getvalue()
+
 @csrf_exempt
 def register(request):
     if request.method == "POST":
@@ -23,14 +36,15 @@ def register(request):
         photo = request.FILES.get("photo")
 
         if name and photo:
-            # Створюємо нового користувача в Django
-            user = User.objects.create_user(
-                username=name, password="temporary_password"
-            )
+            # Нормалізація розміру фото
+            photo_content = photo.read()
+            resized_photo_content = resize_image(photo_content)
+
+            # Створюємо нового користувача
+            user = User.objects.create_user(username=name, password="temporary_password")
 
             # Зберігаємо фото користувача
-            photo_content = photo.read()
-            user_photo = UserPhoto(user=user, photo=photo_content)
+            user_photo = UserPhoto(user=user, photo=resized_photo_content)
             user_photo.save()
 
             # Автоматичний вхід користувача після реєстрації
@@ -47,10 +61,8 @@ def login(request):
         photo = request.FILES.get("photo")
 
         if photo:
-            login_image_content = photo.read()
-            login_image = face_recognition.load_image_file(
-                io.BytesIO(login_image_content)
-            )
+            login_image_content = resize_image(photo.read())
+            login_image = face_recognition.load_image_file(io.BytesIO(login_image_content))
             login_face_encodings = face_recognition.face_encodings(login_image)
 
             if not login_face_encodings:
@@ -61,20 +73,11 @@ def login(request):
                 registered_image_content = user_photo.photo
 
                 try:
-                    registered_image = face_recognition.load_image_file(
-                        io.BytesIO(registered_image_content)
-                    )
-                    registered_face_encodings = face_recognition.face_encodings(
-                        registered_image
-                    )
+                    registered_image = face_recognition.load_image_file(io.BytesIO(registered_image_content))
+                    registered_face_encodings = face_recognition.face_encodings(registered_image)
 
-                    if (
-                        len(registered_face_encodings) > 0
-                        and face_recognition.compare_faces(
-                            registered_face_encodings, login_face_encodings[0]
-                        )[0]
-                    ):
-                        # Якщо обличчя співпадає, автоматично виконуємо вхід
+                    if len(registered_face_encodings) > 0 and face_recognition.compare_faces(registered_face_encodings, login_face_encodings[0])[0]:
+                        # Вхід у систему
                         user = user_photo.user
                         auth_login(request, user)
                         return JsonResponse({"success": True, "name": user.username})
